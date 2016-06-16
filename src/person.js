41942dc3b5189674b9ceb2e880820c79ee145f73
Nam Luong <nam.luongdaiquoc@harveynash.vn>
2016-06-16 16:54:44 +0700

    first exercise

---
+++ src/person.js
@@ -0,0 +1,31 @@
+var person = function (gender, firstName, lastName){
+	var gender, firstName, lastName;
+	
+	this.setFirstName = function(newFirstName){
+		firstName = newFirstName;
+	};
+
+	this.getFirstName = function(){
+		return firstName;
+	};
+
+	this.setLastName = function(newLastName){
+		lastName = newLastName;
+	};
+
+	this.getLastName = function(){
+		return lastName;
+	};
+
+	this.setGender = function(newGender){
+		gender = newGender.toLowerCase();
+	};
+
+	this.getGender = function(){
+		return gender || 'male';
+	};
+
+	this.getFullName = function () {
+		return (gender.toLowerCase() === 'male' ? 'Mr' : "Ms") + '.' + firstName + lastName;
+	};
+};
---
+++ test/person.spec.js
@@ -0,0 +1,94 @@
+describe('Person must have its own method', function () {
+	var people;
+
+	beforeEach(function() {
+		people = new person();
+	});
+
+
+	it('should define setGender method', function () {
+		expect(people.setGender).toBeDefined()
+	});
+
+	it('should define getGender method', function () {
+		expect(people.getGender).toBeDefined()
+	});
+
+	it('should define setFirstName method', function () {
+		expect(people.setFirstName).toBeDefined();
+	});
+
+	it('should define getFirstName method', function () {
+		expect(people.getFirstName).toBeDefined();
+	});
+
+	it('should define setLastName method', function () {
+		expect(people.setLastName).toBeDefined();
+	});
+
+	it('should define getLastName method', function () {
+		expect(people.getLastName).toBeDefined();
+	});
+
+	it('should define getFullName method', function () {
+		expect(people.getFullName).toBeDefined();
+	});
+});
+
+describe('Person-Gender', function() {
+	var people;
+
+	beforeEach(function() {
+		people = new person();
+	});
+
+	it('should be Male in default', function () {
+		expect(people.getGender()).toEqual('male');
+	});
+
+	it('should be Female', function () {
+		people.setGender('female');
+		expect(people.getGender()).toEqual('female');
+	});
+});
+
+describe('Person-FirstName', function() {
+	var people;
+
+	beforeEach(function() {
+		people = new person();
+	});
+
+	it('should be Nam in firstname', function () {
+		people.setFirstName('Nam');
+		expect(people.getFirstName()).toEqual('Nam');
+	});
+});
+
+describe('Person-LastName', function() {
+	var people;
+
+	beforeEach(function() {
+		people = new person();
+	});
+
+	it('should be Luong in lastname', function () {
+		people.setLastName('Luong');
+		expect(people.getLastName()).toEqual('Luong');
+	});
+});
+
+describe('Person-FullName', function() {
+	var people;
+
+	beforeEach(function() {
+		people = new person();
+	});
+
+	it('should be Ms Nam Luong', function () {
+		people.setGender('Female');
+		people.setFirstName('Nam');
+		people.setLastName('Luong');
+		expect(people.getFullName()).toEqual('Ms.NamLuong');
+	});
+});
