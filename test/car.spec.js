69e3da9b615f3a8822a0defbb12e14384baca082
Nam Luong <nam.luongdaiquoc@harveynash.vn>
2016-06-20 16:10:21 +0700

    using callthrough in mock to make sure the function is excuted actually

--- test/car.spec.js
+++ test/car.spec.js
@@ -27,21 +27,16 @@ describe('Cars logic check', function() {
 
 	it('addPassenger() should be called to add Person', function() {
 		var person = new Person();
-		spyOn(car, 'addPassenger');
+		spyOn(car, 'addPassenger').and.callThrough();
 		car.addPassenger(person);
-		//console.log("XXXXXXXXXXXXXX",car.getNumberOfPassenger());  = 0
+		//using callThrough to make sure that addPassenger is called actually
 		expect(car.addPassenger).toHaveBeenCalledWith(person); 
+		expect(car.getNumberOfPassenger()).toEqual(1);
 	});
 
 	it('No of Pass should be 0', function() {
 		expect(car.getNumberOfPassenger()).toEqual(0);
 	});
-
-	it('No of Pass should be 1', function() {
-		var person = new Person();
-		car.addPassenger(person);
-		expect(car.getNumberOfPassenger()).toEqual(1);
-	});	
 
 	it('No of Pass over the max', function() {
 		var person1 = new Person();
@@ -49,32 +44,23 @@ describe('Cars logic check', function() {
 		var person2 = new Person();
 		car.addPassenger(person2);
 		var person3 = new Person();
-		expect(function(){car.addPassenger(person3);}).toThrow( new Error("Over max number of passengers"));		
+		expect(function(){car.addPassenger(person3);}).toThrow(new Error("Over max number of passengers"));		
 	});
 
-	it('No of men', function() {
+	it('No of men - getGender() of Person should be called', function() {
 		var person1 = new Person();
 		person1.setGender('male');
 		car.addPassenger(person1);
 		var person2 = new Person();
 		person2.setGender('female');
 		car.addPassenger(person2);
-		expect(car.getNumberOfMen()).toEqual(1);		
-	});
 
-	it('getGender() of Person should be called', function() {
-		var person1 = new Person();
-		person1.setGender('male');
-		car.addPassenger(person1);
-		var person2 = new Person();
-		person2.setGender('female');
-		car.addPassenger(person2);
-		spyOn(person1, 'getGender');
-		spyOn(person2, 'getGender');
-		car.getNumberOfMen();
+		//using callThrough to make sure that getGender is called actually
+		spyOn(person1, 'getGender').and.callThrough();
+		spyOn(person2, 'getGender').and.callThrough();
 
+		expect(car.getNumberOfMen()).toEqual(1);
 		expect(person1.getGender).toHaveBeenCalled(); 
 		expect(person2.getGender).toHaveBeenCalled();
 	});
-
 });	
